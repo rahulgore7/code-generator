00abcfb26b3517359f2d9966efebaf5d0e5d6ae2
Rahul Gore <rahulgore575@gmail.com>
2024-01-10 04:53:49 +0530

    backend cors congigured

--- server/app.js
+++ server/app.js
@@ -6,15 +6,14 @@ const cors = require('cors')
 
 const app = express();
 const PORT = 4000;
-const corsOrigin = {
-    origin: 'http://localhost:3000',
-    credentials: true,
-    optionSuccessStatus: 200
-}
-app.use(cors(corsOrigin));
+
 
 mongoose.connect('mongodb+srv://rahulgore575:Samu@4321@cluster0.xno60mp.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
-
+app.use(cors({
+    origin: ["*"],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true
+}));
 app.use(bodyParser.json());
 app.use(routes);
 
